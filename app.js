4975931324c2b5de39561f1df2b60484899e77b8
Mona Lisa <santifranquez@gmail.com>
2022-05-21 00:54:44 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+// NAV
+const nav = document.querySelector('nav');
+  window.addEventListener('scroll', function() {
+
+    if(window.scrollY > 400) {
+      nav.classList.add('bg-fijado', 'shadow')
+    } else {
+      nav.classList.remove('bg-fijado', 'shadow')
+    }
+  
+    // console.log(this.window.screen.availWidth)
+  })
+
+
+
+
+// Validando el formulario
+
+
+
+
